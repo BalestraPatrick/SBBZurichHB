a117b9d68d7494af14af5460fe226d61b0504e59
Patrick Balestra <me@patrickbalestra.com>
2016-09-16 21:15:35 +0200

    Add initial project

---
+++ SBBZurichNotifier/SBBZurichNotifier/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SBBZurichNotifier
+//
+//  Created by Patrick Balestra on 9/16/16.
+//  Copyright © 2016 Patrick Balestra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
