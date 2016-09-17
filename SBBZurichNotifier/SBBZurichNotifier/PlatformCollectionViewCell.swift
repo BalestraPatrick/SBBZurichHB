fe9bbf21969b8df17e685c6bbe52ace72e5417ac
Patrick Balestra <me@patrickbalestra.com>
2016-09-17 02:37:16 +0200

    First UI draft

---
+++ SBBZurichNotifier/SBBZurichNotifier/PlatformCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PlatformCollectionViewCell.swift
+//  SBBZurichNotifier
+//
+//  Created by Patrick Balestra on 9/17/16.
+//  Copyright © 2016 Patrick Balestra. All rights reserved.
+//
+
+import UIKit
+
+class PlatformCollectionViewCell: UICollectionViewCell {
+
+    public static let reuseIdentifier = "PlatformCollectionViewCell"
+    
+    @IBOutlet  weak var titleLabel: UILabel!
+}
--- SBBZurichNotifier/SBBZurichNotifier/ViewController.swift
+++ SBBZurichNotifier/SBBZurichNotifier/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,5 +22,26 @@ class ViewController: UIViewController {
                 })
             }
         }
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PlatformCollectionViewCell.reuseIdentifier, for: indexPath) as! PlatformCollectionViewCell
+        cell.titleLabel.text = "Miao"
+        return cell
     }
 }
